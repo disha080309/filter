4e142eff51b9cfef686d11890485ca0552b08eb2
disha080309 <84317794+disha080309@users.noreply.github.com>
2022-04-19 12:21:45 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+function preload(){
+
+}
+function setup(){
+canvas=createCanvas(300,300);
+canvas.center();
+video=createCapture(VIDEO);
+video.hide();
+}
+function draw(){
+    image(video,0,0,300,300);
+}
+function take_snapshot(){
+    save("my_filter_selfie.png");
+}
